865eb7cf638fc807c3b23f5a4a88ebb96a618bdc
Samuel <samuelh08.shm@gmail.com>
2022-11-17 19:50:35 -0500

    Move developer name to right side

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,8 +11,10 @@ export default function Footer() {
       width="100%"
       padding="10px 10px 0px 10px"
       bgcolor="black"
+      display="flex"
+      justifyContent="end"
     >
-      <Typography variant="h6" color="white">
+      <Typography variant="h6" color="white" marginRight={5}>
         Samuel Hincapié Monsalve
       </Typography>
     </Box>
